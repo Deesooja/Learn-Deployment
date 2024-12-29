9102af89b48c0ac02698feb1a3ceedfb9ff03dfd
Deesooja <deesooja24dp@gmail.com>
2024-12-29 18:07:11 +0530

    added new api - get user repoer - 2

--- Myapp/serializers.py
+++ Myapp/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from Myapp.models import Todo
+from Myapp.models import Todo, UserRecord
 
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todo
+        fields = "__all__"
+
+class UserRecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserRecord
         fields = "__all__"
--- Myapp/urls.py
+++ Myapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from Myapp.views import GetTodoList, CreateTodo
+from Myapp.views import GetTodoList, CreateTodo, GetUserReport
 
 urlpatterns = [
     path("get-list", GetTodoList.as_view()),
     path("<int:user_id>/create-todo", CreateTodo.as_view()),
+    path("<int:user_id>/user-report", GetUserReport.as_view()),
 ]
